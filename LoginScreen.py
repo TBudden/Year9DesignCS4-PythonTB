45e0afee6c083cc1ad1a6b284b57b09ad36be66c
Taylor Budden <taylor.budden@Y22-designTaylor-Budden.local>
2018-11-22 13:30:10 -0500

    Example Programs for Unit 1 code

--- LoginScreen.py
+++ LoginScreen.py
@@ -1,8 +1,20 @@
 import tkinter as tk
 
-
 root = tk.Tk()
 
+labUN = tk.Label(root, text = "User Name")
+labUN.pack()
 
+entUN = tk.Entry(root)
+entUN.pack(ipadx = 10)
+
+labPassword = tk.Label(root, text = "Password")
+labPassword.pack()
+
+entPassword = tk.Entry(root, show = "*")
+entPassword.pack()
+
+btnSubmit = tk.Button (root, text = "Submit")
+btnSubmit.pack()
 
 root.mainloop()
