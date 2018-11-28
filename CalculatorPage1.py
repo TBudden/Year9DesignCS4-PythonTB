1cb3a1de144e7ffbe138a0f5f6ee8caed29af29f
Taylor Budden <taylor.budden@Y22-designTaylor-Budden.local>
2018-11-28 14:11:17 -0500

    Push- Nov 28

--- CalculatorPage1.py
+++ CalculatorPage1.py
@@ -1,1 +1,30 @@
 import tkinter as tk
+import math
+
+def change(*args):
+	print("running change")
+	print(var.get())
+
+def submit():
+
+	print("Submit")
+
+root = tk.Tk()
+root.title("Calculator Home Page")
+
+Equations = [
+"Slope Y-Int Form(Y=mx+b)",
+ "Slope(y2-y1/x2-x1)"
+ ]
+
+var = tk.StringVar(root)
+var.set(Equations[0])
+var.trace("w",change)
+
+dropDownMenu = tk.OptionMenu(root,var, Equations[0],Equations[1])
+dropDownMenu.pack()
+
+btn = tk.Button(root, text="submit", command=submit)
+btn.pack()
+
+root.mainloop()
